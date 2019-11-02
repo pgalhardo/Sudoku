5c11e08e919284a958176484697b5ba4752783cc
pgalhardo <pedromgalhardo@tecnico.ulisboa.pt>
2019-11-02 02:31:39 +0000

    Add support for hour-long games

--- Sudoku/Support Views/TimerView.swift
+++ Sudoku/Support Views/TimerView.swift
@@ -11,21 +11,36 @@ import SwiftUI
 
 struct TimerView : View {
     
+	@State private var _hour: Int = 0
 	@State private var _min: Int = 0
 	@State private var _sec: Int = 0
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let _timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-		Text(String(format: "%02d:%02d", arguments: [self._min, self._sec]))
+		Text(String(format: "%@%02d:%02d", arguments: [self.hours(),
+													   self._min,
+													   self._sec]))
 			.font(.custom("CaviarDreams-Bold", size: Screen.cellWidth / 2))
-            .onReceive(timer) { _ in
+            .onReceive(_timer) { _ in
                 self._sec += 1
 				if self._sec == 60 {
 					self._min += 1
 					self._sec = 0
 				}
+				
+				if self._min == 60 {
+					self._hour += 1
+					self._min = 0
+				}
             }
     }
+	
+	func hours() -> String {
+		if self._hour != 0 {
+			return String(format: "%02d:", arguments: [self._hour])
+		}
+		return String("")
+	}
 }
 
 
