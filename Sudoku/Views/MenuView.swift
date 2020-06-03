5804f367bdfcc32a7fdfd7193a1ce8716b3c9008
pgalhardo <pedromgalhardo@tecnico.ulisboa.pt>
2020-06-03 22:50:38 +0100

    Refactor ActivityIndicator.

--- Sudoku/Views/MenuView.swift
+++ Sudoku/Views/MenuView.swift
@@ -357,19 +357,41 @@ struct ActivityIndicator: View {
 			ForEach(0 ..< 5) { index in
 				Group {
 					Circle()
-						.frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
-						.scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
-						.offset(y: geometry.size.width / 10 - geometry.size.height / 2)
-				}.frame(width: geometry.size.width, height: geometry.size.height)
-					.rotationEffect(!self.isAnimating ? .degrees(0) : .degrees(360))
-					.animation(Animation
-						.timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5)
-						.repeatForever(autoreverses: false))
+						.frame(width: geometry.size.width / 5,
+							   height: geometry.size.height / 5)
+						.scaleEffect(self.scaleEffect(index: index))
+						.offset(y: self.offset(geometry: geometry))
+				}
+				.frame(width: geometry.size.width,
+					   height: geometry.size.height)
+					.rotationEffect(self.rotation())
+				.animation(Animation
+					.timingCurve(0.5,
+								 0.15 + Double(index) / 5,
+								 0.25,
+								 1,
+								 duration: 1.5)
+					.repeatForever(autoreverses: false)
+				)
 			}
 		}
 		.aspectRatio(1, contentMode: .fit)
 		.onAppear {
 			self.isAnimating = true
 		}
+	}
+	
+	func scaleEffect(index: Int) -> CGFloat {
+		let animatedEffect: CGFloat = 1 - CGFloat(index) / 5
+		let frozenEffect: CGFloat = 0.2 + CGFloat(index) / 5
+		return !self.isAnimating ? animatedEffect : frozenEffect
+	}
+	
+	func offset(geometry: GeometryProxy) -> CGFloat {
+		return geometry.size.width / 10 - geometry.size.height / 2
+	}
+	
+	func rotation() -> Angle {
+		return !self.isAnimating ? .degrees(0) : .degrees(360)
 	}
 }
